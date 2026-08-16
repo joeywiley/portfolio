c6b4fbc04ae1f4e3f451f3e333f9cd1e94714bd3
Joey Wiley <joey.w.wiley@gmail.com>
2026-08-15 20:21:42 -0700

    fix: mobile viewport

--- src/App.vue
+++ src/App.vue
@@ -71,9 +71,11 @@ onUnmounted(() => {
 <style lang="scss" scoped>
 .particle-canvas {
   position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
   z-index: -10;
-  width: 100lvw;
-  height: 100lvh;
 }
 
 .content-wrapper {
